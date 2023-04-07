38acdbec82cd511c80be5b1e992d7cebab3ef525
caleb jephuneh <calebjephunneh@gmail.com>
2023-04-07 18:24:03 +0300

    more

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,18 +1,10 @@
 import React from 'react'
 import Navbar from './Navbar'
-import BannerBackground from '../assets/Vector 1.png'
 
 function Home() {
   return (
     <div className='home-container'>
       <Navbar />
-      <div className='home-container'>
-        <div className='Banner-background-cont'>
-            <img src={BannerBackground} alt="" />
-        </div>
-        
-
-      </div>
     </div>
   )
 }
